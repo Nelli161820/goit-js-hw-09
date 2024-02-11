65f3faebee5e8b2cccf3c5b9080cb706f7e1db96
Nelli161820 <124123144+Nelli161820@users.noreply.github.com>
2024-02-11 14:30:43 +0400

    hw-js-9-error correction

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,46 +1,57 @@
+
 // Отримуємо елементи форми
-let form = document.querySelector('.feedback-form');
-let emailInput = form.elements.email;
-let messageInput = form.elements.message;
 
-// Перевіряємо, чи є збережені дані у локальному сховищі
-let savedData = localStorage.getItem('feedback-form-state');
+document.addEventListener('DOMContentLoaded', (event) => {
+  let form = document.querySelector('.feedback-form');
+  if (form) {
+    let emailInput = form.elements.email;
+    let messageInput = form.elements.message;
 
-if (savedData) {
-  // Якщо дані збережені, перетворюємо їх з JSON-формату і заповнюємо поля форми
-  let formData = JSON.parse(savedData);
-  emailInput.value = formData.email || '';
-  messageInput.value = formData.message || '';
-}
 
-// Додаємо обробник подій 'input' для збереження значень у локальне сховище
-form.addEventListener('input', function (event) {
-  // Зберігаємо значення полів у об'єкт
-  let formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  // Зберігаємо об'єкт у локальне сховище
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    // Перевіряємо, чи є збережені дані у локальному сховищі
+    let savedData = localStorage.getItem('feedback-form-state');
+
+    if (savedData) {
+      // Якщо дані збережені, перетворюємо їх з JSON-формату і заповнюємо поля форми
+      let formData = JSON.parse(savedData);
+      emailInput.value = formData.email || '';
+      messageInput.value = formData.message || '';
+    }
+
+    // Додаємо обробник подій 'input' для збереження значень у локальне сховище
+    form.addEventListener('input', function (event) {
+      // Зберігаємо значення полів у об'єкт
+      let formData = {
+        email: emailInput.value.trim(),
+        message: messageInput.value.trim(),
+      };
+      // Зберігаємо об'єкт у локальне сховище
+      localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+    });
+
+    // Додаємо обробник подій 'submit' для форми
+    form.addEventListener('submit', function (event) {
+      // Зупиняємо стандартну обробку події
+      event.preventDefault();
+
+      // Отримуємо поточні значення полів
+    if (emailInput.value.trim() === '' || messageInput.value.trim() === '') {
+      alert('Будь ласка, заповни поля');
+      return;
+    }
+
+    // Виводимо поточний стан форми в консоль
+    console.log({
+      email: emailInput.value.trim(),
+      message: messageInput.value.trim(),
+    });
+
+      // Очищаємо поля форми
+      form.reset();
+
+      // Очищаємо локальне сховище
+      localStorage.removeItem('feedback-form-state');
+    });
+  }
 });
 
-// Додаємо обробник подій 'submit' для форми
-form.addEventListener('submit', function (event) {
-  // Зупиняємо стандартну обробку події
-  event.preventDefault();
-
-  // Отримуємо поточні значення полів
-  let formData = {
-    email: emailInput.value.trim(),
-    message: messageInput.value.trim(),
-  };
-
-  // Виводимо дані у консоль
-  console.log(formData);
-
-  // Очищаємо поля форми
-  form.reset();
-
-  // Очищаємо локальне сховище
-  localStorage.removeItem('feedback-form-state');
-});
